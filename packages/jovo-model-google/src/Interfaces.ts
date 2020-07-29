222243836a83fb47dd08fc42eb3d24b70eefeed2
rubenaeg <email@aegerter.tech>
2020-07-29 17:53:57 +0200

    ✨ Improve JovoModelGoogleValidator

--- packages/jovo-model-google/src/Interfaces.ts
+++ packages/jovo-model-google/src/Interfaces.ts
@@ -1,11 +1,11 @@
 export interface GAIntent {
-    trainingPhrases: string[];
-    parameters?: { name: string; type: { name: string } }[];
+  trainingPhrases: string[];
+  parameters?: { name: string; type: { name: string } }[];
 }
 
 export interface GAInput {
-    synonym: {
-        entities: { [key: string]: { synonyms: string[] } };
-        matchType?: string;
-    };
+  synonym: {
+    entities: { [key: string]: { synonyms: string[] } };
+    matchType?: string;
+  };
 }
--- packages/jovo-model-google/src/index.ts
+++ packages/jovo-model-google/src/index.ts
@@ -1,1 +1,1 @@
-export * from "./JovoModelGoogle";
+export * from './JovoModelGoogle';
